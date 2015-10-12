4db37a4cd5ae8d3c5259d03d9ef940d347177999
yutayokoi <dezirus.novajn.iojn@gmail.com>
2015-10-12 22:08:16 +0900

    レイアウト修正

--- app/src/main/java/com/example/yuta/tsubuyakiapp/DtoTsubuyaki.java
+++ app/src/main/java/com/example/yuta/tsubuyakiapp/DtoTsubuyaki.java
@@ -4,11 +4,8 @@ package com.example.yuta.tsubuyakiapp;
  * Created by yuta on 15/10/12.
  */
 public class DtoTsubuyaki {
-    /** long:intの拡張版 */
-
-    /* ID */
+    /** ID */
     public long id;
-
-    /* コメント */
+    /** コメント */
     public String comment;
 }
