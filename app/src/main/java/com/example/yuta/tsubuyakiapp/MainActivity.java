af8c632f87d3801fa9827c10d2c445b27b0b5cbd
yutayokoi <dezirus.novajn.iojn@gmail.com>
2015-10-12 21:50:36 +0900

    MainActivity修正

--- app/src/main/java/com/example/yuta/tsubuyakiapp/MainActivity.java
+++ app/src/main/java/com/example/yuta/tsubuyakiapp/MainActivity.java
@@ -9,60 +9,53 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /** つぶやき表示用レイアウト */
+    // ----------fields----------
+
+    /** つぶやき表示用リストビュー */
     private ListView listView;
     /** つぶやき入力用エディットテキスト */
     private EditText editText;
+
+    // ----------methods----------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // レイアウトよりビューを取得
         listView = (ListView) findViewById(R.id.listView);
         editText = (EditText) findViewById(R.id.editText);
 
-        /**
-         * ArrayAdapterの作成、リストビューにセットする
-         * 第一引数：コンテキスト
-         * 第二引数：レイアウト
-         * 第三引数：ArrayAdapterにセットするデータ
-         */
+        // ArrayAdapterを作成し、ListViewにデータをセットする
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                DaoTsubuyaki.findAll(getApplicationContext()
-                ));
-
-        /** リストビューにデータをセット */
+                this, android.R.layout.simple_list_item_1, DaoTsubuyaki.findAll(getApplicationContext()));
         listView.setAdapter(adapter);
 
+        // つぶやきボタンにリスナーを設定する
         findViewById(R.id.commitButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /** editTextに入力されている文字列を取得 */
+                // 入力されているつぶやきを取得し、テーブルに追加
                 String comment = editText.getText().toString();
-
-                /** テーブルにデータを追加 */
                 DaoTsubuyaki.insert(getApplicationContext(), comment);
 
-                /** EditTextに入力されている文字列の初期化 */
+                // エディットテキストを初期化
                 editText.setText("");
 
-                /** 再度、テーブルのデータを取得し、ListViewを更新する */
+                // ListViewを更新
                 updateListView();
             }
         });
     }
 
+    /**
+     * ListViewを更新
+     */
     private void updateListView() {
-        /** データを取り出す */
-        ArrayAdapter<String> adapter = (ArrayAdapter) listView.getAdapter();
-
-        /** 入っているデータを消す */
+        ArrayAdapter<String> adapter = (ArrayAdapter<String>) listView.getAdapter();
         adapter.clear();
-
-        /** 再度データベースからデータを取得して入れ直す */
         adapter.addAll(DaoTsubuyaki.findAll(getApplicationContext()));
     }
+
 }
